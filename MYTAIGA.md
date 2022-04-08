090788fb9fd7063b2aec33cbb04817ff6483e497
George BV <egor.workroom@gmail.com>
2022-04-08 06:03:14 +0300

    upded the configs and guide

--- MYTAIGA.md
+++ MYTAIGA.md
@@ -1,6 +1,8 @@
-* setup env variables `docker-compose.mytaiga.env`
+* setup env variables `mytaiga.env` based on `mytaiga.env.example`
   * only for main variables `MYTAIGA_SECRET_KEY`, `MYTAIGA_SITE_SCHEME`, `MYTAIGA_SITE_SCHEME_WS`, `MYTAIGA_SITE_DOMAIN`, `MYTAIGA_SITE_SUBPATH`. The main values will be substituted in the taiga variables where it's needed.
   * (optional) envs for email
-* launch taiga `docker-compose -f docker-compose.mytaiga.yml up -d`
+* check envs `./mytaiga.sh check-envs`
+* launch taiga `./mytaiga.sh run`
+* create superuser `./mytaiga.sh manage createsuperuser`
 * TODO: backup DB
 * TODO: backup attached files
---
+++ mytaiga.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+case "$1" in
+    "check-envs" )
+        echo "Launch taiga backend"
+        exec docker-compose -f docker-compose.mytaiga.yml run --rm check-envs
+    ;;
+    "run" )
+        echo "Launch taiga backend"
+        exec docker-compose -f docker-compose.mytaiga.yml up -d taiga-gateway taiga-protected taiga-async
+    ;;
+    "restart-back" )
+        exec docker-compose -f docker-compose.mytaiga.yml up -d taiga-back taiga-async
+    ;;
+    "manage" )
+        echo "Taiga manage command: ${@:2}"
+        exec docker-compose -f docker-compose.mytaiga.yml run --rm taiga-manage ${@:2}
+    ;;
+    * | "--help" )
+        echo "Command '$@' not found"
+    ;;
+esac
